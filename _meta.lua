d64f827a7d7556c8cbf3eead79d67b88c02d081b
Euphoriyy <jstkidding@mail.com>
2026-05-17 19:23:51 -0400

    chore: bump plugin version to 0.0.3

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     fullname = _("Derainbowify"),
     description = _(
         [[Removes the rainbow patterns created by the color filter on Kaleido 3 e-ink screens.]]),
-    version = "0.0.2",
+    version = "0.0.3",
 }
