84c1c5eb15f2ee3bd31271d10bf7c70b78e75f46
Euphoriyy <jstkidding@mail.com>
2026-06-07 18:22:17 -0400

    fix(meta): remove deprecated name field

--- _meta.lua
+++ _meta.lua
@@ -1,6 +1,5 @@
 local _ = require("gettext")
 return {
-    name = "derainbowify",
     fullname = _("Derainbowify"),
     description = _(
         [[Removes the rainbow patterns created by the color filter on Kaleido 3 e-ink screens.]]),
